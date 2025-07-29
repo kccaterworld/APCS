0252b6ded915076887a67c29ccb4d149ffd132ee
Kam <kc@caterworld.xyz>
2025-07-29 10:54:07 -0400

    first commit

---
+++ Hello.java
@@ -0,0 +1,14 @@
+public class Hello {
+    public static double roundNoPlace(double value) {
+        double placeUp = value * 10;
+        
+        double rounded = value;
+        return rounded;
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        double number =  6.457;
+        int roundy = (int) number;
+        System.out.println(roundy);
+    }
+}
